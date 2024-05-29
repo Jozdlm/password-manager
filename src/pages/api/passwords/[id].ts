d5d1e5e659be45075aea740d503bcf1ee795a642
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-29 16:28:27 -0600

    Allows client to retrieve a password by userId and PasswordId

--- src/pages/api/passwords/[id].ts
+++ src/pages/api/passwords/[id].ts
@@ -1,23 +1,29 @@
 import type { APIRoute } from "astro";
-import { getPasswordById } from "../../../lib/repositories/passwords";
+import { supabase } from "../../../lib/supabase";
 
-export const GET: APIRoute = ({ params }) => {
-  const id = params.id as string;
-  const password = getPasswordById(id);
+export const GET: APIRoute = async ({ params, redirect }) => {
+  const { session } = (await supabase.auth.getSession()).data;
 
-  if (!password) {
-    return new Response(
-      JSON.stringify({
-        error: "Record not found",
-      }),
-      {
-        status: 404,
-        statusText: "Not found",
-      }
-    );
+  if (!session) {
+    redirect("/login");
   }
 
-  return new Response(JSON.stringify(password), {
+  const userId = session!.user.id;
+  const id = params.id as string;
+
+  let { data: passwords, error } = await supabase
+    .from("passwords")
+    .select("*")
+    .eq("id", id)
+    .eq("user_id", userId);
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+
+  return new Response(JSON.stringify(passwords![0]), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
