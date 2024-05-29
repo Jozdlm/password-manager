983b86cc73dca6676fcc062d8dc7d848c5686ef3
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-29 15:58:48 -0600

    Allows client to insert a new password in to db

--- src/pages/api/passwords.ts
+++ src/pages/api/passwords.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from "astro";
 import { getPasswords } from "../../lib/repositories/passwords";
+import { supabase } from "../../lib/supabase";
+import type { InsertPassword } from "../../lib/interfaces/password";
 
 export const GET: APIRoute = () => {
   const passwords = getPasswords();
@@ -9,4 +11,45 @@ export const GET: APIRoute = () => {
       "Content-Type": "application/json",
     },
   });
+};
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+
+  const url = formData.get("url")?.toString();
+  const username = formData.get("username")?.toString();
+  const password = formData.get("password")?.toString();
+  const note = formData.get("note")?.toString();
+  const userId = formData.get("userId")?.toString();
+
+  if (!url || !username || !password) {
+    return new Response(
+      JSON.stringify({ error: "Url, username and password are required" }),
+      { status: 400 }
+    );
+  }
+
+  if (!userId) {
+    return new Response(JSON.stringify({ error: "An user ID are required" }), {
+      status: 400,
+    });
+  }
+
+  const newPassword: InsertPassword = {
+    url,
+    username,
+    password,
+    note: note || "",
+    user_id: userId,
+  };
+
+  const { error } = await supabase.from("passwords").insert(newPassword);
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+
+  return redirect("/");
 };
