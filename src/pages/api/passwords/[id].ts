68949eab9ff2ababb86d5b3284d957a5cbeb9125
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-31 20:15:39 -0600

    Allows client to update a password by id

--- src/pages/api/passwords/[id].ts
+++ src/pages/api/passwords/[id].ts
@@ -31,6 +31,41 @@ export const GET: APIRoute = async ({ params, redirect }) => {
   });
 };
 
+export const POST: APIRoute = async ({ params, redirect, request }) => {
+  const formData = await request.formData();
+  const id = params.id;
+
+  const url = formData.get("url")?.toString();
+  const username = formData.get("username")?.toString();
+  const password = formData.get("password")?.toString();
+  const note = formData.get("note")?.toString();
+
+  if (!username || !password) {
+    return new Response("Missing required fields", {
+      status: 400,
+    });
+  }
+
+  if (!id) {
+    return new Response("Cannot find password", {
+      status: 404,
+    });
+  }
+
+  const { error } = await supabase
+    .from("passwords")
+    .update({ url, username, password, note })
+    .eq("id", id);
+
+  if (error) {
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+
+  return redirect(`/app/${id}`);
+};
+
 export const DELETE: APIRoute = async ({ params, redirect }) => {
   const id = params.id as string;
 
