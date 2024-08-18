2538ec0db4bb9fe60d26c6ea1bbfd945eb54abb0
Jozuan Lopez <jozuanlopez@gmail.com>
2024-08-18 16:51:57 -0600

    Add react and shadcn/ui to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
