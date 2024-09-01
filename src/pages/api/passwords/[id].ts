d8e1beef75974c50ab203c79dbbb56cda73ca97b
Jozuan Lopez <jozuanlopez@gmail.com>
2024-08-31 21:29:09 -0600

    Removes unused code and use server action

--- src/pages/api/passwords/[id].ts
+++ src/pages/api/passwords/[id].ts
@@ -1,36 +1,6 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 
-export const GET: APIRoute = async ({ params, redirect }) => {
-  const { session } = (await supabase.auth.getSession()).data;
-
-  if (!session) {
-    redirect("/login");
-  }
-
-  const userId = session!.user.id;
-  const id = params.id as string;
-
-  let { data: passwords, error } = await supabase
-    .from("passwords")
-    .select("*")
-    .eq("id", id)
-    .eq("user_id", userId);
-
-  if (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-    });
-  }
-
-  return new Response(JSON.stringify(passwords![0]), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
-
 export const POST: APIRoute = async ({ params, redirect, request }) => {
   const formData = await request.formData();
   const id = params.id;
