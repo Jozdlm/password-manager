5e297671ecadf327a7c02fee0ff7c95b642b1cde
Jozuan Lopez <jozuanlopez@gmail.com>
2024-08-31 21:26:10 -0600

    Use a server action instead of API endpoint

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 import type { Password } from "@/lib/interfaces/password";
 import { supabase } from "@/lib/supabase";
 import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
 
 export const server = {
   getPasswords: defineAction({
@@ -18,6 +19,43 @@ export const server = {
         throw new ActionError({ code: "BAD_REQUEST", message: error.message });
 
       return passwords as Password[];
+    },
+  }),
+  getPasswordById: defineAction({
+    input: z.string(),
+    handler: async (passwordId) => {
+      const { data, error: sessionError } = await supabase.auth.getSession();
+
+      if (sessionError)
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: sessionError.message,
+        });
+
+      if (!data.session)
+        throw new ActionError({
+          code: "NOT_FOUND",
+          message: "No founded session",
+        });
+
+      const userId = data.session.user.id;
+
+      let { data: passwords, error } = await supabase
+        .from("passwords")
+        .select("*")
+        .eq("id", passwordId)
+        .eq("user_id", userId);
+
+      if (error)
+        throw new ActionError({ code: "BAD_REQUEST", message: error.message });
+
+      if (!passwords)
+        throw new ActionError({
+          code: "NOT_FOUND",
+          message: "Password not founded",
+        });
+
+      return passwords[0] as Password;
     },
   }),
 };
