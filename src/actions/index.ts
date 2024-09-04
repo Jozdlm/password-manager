2392a74105f2de0f10fb029a7a845b99a876bff4
Jozuan Lopez <jozuanlopez@gmail.com>
2024-09-03 19:56:33 -0600

    Switch from API endpoint to server action and validate the form

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,50 @@
-import type { InsertPassword, Password } from "@/lib/interfaces/password";
 import { supabase } from "@/lib/supabase";
 import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro:schema";
-import * as PasswordActions from "./password.action";
+import * as passwordActions from "./password.action";
 
 export const server = {
-  ...PasswordActions,
+  ...passwordActions,
+  login: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z
+        .string({
+          message: "El correo no puede estar vacío",
+        })
+        .email({
+          message: "El correo proporcionado no es válido",
+        }),
+      password: z
+        .string({
+          message: "El campo contraseña es requerido",
+        })
+        .trim()
+        .min(6, {
+          message: "La contraseña debe contener un mínimo de 6 carácteres",
+        }),
+    }),
+    handler: async ({ email, password }, ctx) => {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error)
+        throw new ActionError({ code: "BAD_REQUEST", message: error.message });
+
+      const { access_token, refresh_token } = data.session;
+
+      ctx.cookies.set("sb-access-token", access_token, {
+        path: "/",
+      });
+      ctx.cookies.set("sb-refresh-token", refresh_token, {
+        path: "/",
+      });
+
+      return { success: true };
+    },
+  }),
   logout: defineAction({
     accept: "form",
     handler: (_, ctx) => {
